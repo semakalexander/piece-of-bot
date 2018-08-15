6873477463af19f81f82568e8880db76d9b5cb63
semakalexander <semak.alexander@gmail.com>
2018-08-15 22:55:06 +0300

    fix emoji. now poop is not okay

--- util.js
+++ util.js
@@ -2,14 +2,14 @@ const uniqid = require('uniqid');
 const generateId = () => uniqid();
 
 const thumbs_up_emoji = '❤️';
-const thumbs_down_emoji = '👎🏻';
-const thumbs_okay_emoji = '💩';
+const thumbs_okay_emoji = '👌🏻';
+const thumbs_down_emoji = '💩';
 	
 module.exports = {
   generateId,
   emoji: {
     thumbs_up_emoji,
-    thumbs_down_emoji,
-    thumbs_okay_emoji
+    thumbs_okay_emoji,
+    thumbs_down_emoji
   }
 };
